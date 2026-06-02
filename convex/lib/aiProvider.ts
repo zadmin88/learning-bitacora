051438834dd6cb58e7f41460ecec71ef209e5605
Simon Franco <zaimon88@gmail.com>
2026-06-02 08:01:44 -0500

    change provider

--- convex/lib/aiProvider.ts
+++ convex/lib/aiProvider.ts
@@ -23,7 +23,7 @@ function createCloudflareProvider(
     embeddingDimensions: 768,
 
     async generateText(systemPrompt: string, userPrompt: string) {
-      const res = await fetch(`${baseUrl}/@cf/meta/llama-3-8b-instruct`, {
+      const res = await fetch(`${baseUrl}/@cf/google/gemma-4`, {
         method: "POST",
         headers,
         body: JSON.stringify({
